e78c4384071e8a2d44fd147fc032a24a7f9a1bf6
yuchan <binfotech720@gmail.com>
2024-12-06 05:07:13 +0900

    feat:make folder delete, folder's place list and delete

--- app/main/routes.py
+++ app/main/routes.py
@@ -59,7 +59,28 @@ def create_folder():
     return jsonify({'message': '폴더가 성공적으로 생성되었습니다'})
 
 
-# 장소 추가
+# 폴더 삭제
+@main.route('/remove_folder/<int:folder_id>', methods=['DELETE'])
+@login_required
+def remove_folder(folder_id):
+    folder = PlacesFolder.query.get(folder_id)
+    if folder and folder.owner_id == current_user.id:
+        db.session.delete(folder)
+        db.session.commit()
+        return jsonify({'message': '폴더가 삭제되었습니다'})
+    return jsonify({'message': '폴더를 찾을 수 없습니다'}), 404
+
+
+# 폴더의 장소 가져오기
+@main.route('/get_places/<int:folder_id>', methods=['GET'])
+@login_required
+def get_places(folder_id):
+    places = Place.query.filter_by(folder_id=folder_id).all()
+    place_data = [{'id': place.id, 'name': place.place_name} for place in places]
+    return jsonify({'places': place_data})
+
+
+# 폴더에 장소 추가
 @main.route('/add_place/<folder_id>', methods=['POST'])
 @login_required
 def add_place(folder_id):
@@ -71,6 +92,18 @@ def add_place(folder_id):
     new_place.save_to_db()
 
     return jsonify({'message': '장소가 성공적으로 추가되었습니다'})
+
+
+# 폴더에서 장소 삭제
+@main.route('/remove_place/<int:place_id>', methods=['DELETE'])
+@login_required
+def remove_place(place_id):
+    place = Place.query.get(place_id)
+    if place:
+        db.session.delete(place)
+        db.session.commit()
+        return jsonify({'message': '장소가 삭제되었습니다'})
+    return jsonify({'message': '장소를 찾을 수 없습니다'}), 404
 
 
 # 장소에 정보 추가 (이미지, 설명)
@@ -86,17 +119,3 @@ def add_place_info(place_id):
     place_info.save_to_db()
 
     return jsonify({'message': '장소 정보가 성공적으로 추가되었습니다'})
-
-
-@main.route('/remove_place/<int:folder_id>', methods=['DELETE'])
-@login_required
-def remove_place(folder_id):
-    data = request.get_json()
-    place_name = data.get('name')
-
-    place = Place.query.filter_by(folder_id=folder_id, place_name=place_name).first()
-    if place:
-        db.session.delete(place)
-        db.session.commit()
-        return jsonify({'message': '장소가 성공적으로 삭제되었습니다'})
-    return jsonify({'message': '장소를 찾을 수 없습니다'}), 404
